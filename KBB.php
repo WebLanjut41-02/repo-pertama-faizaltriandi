7f2e4e10e5e5e7ef0cac0dd2127c54ef4eedbff9
faizaltriandi <42662893+faizaltriandi@users.noreply.github.com>
2019-01-18 10:26:25 +0700

    Add files via upload

---
+++ KBB.php
@@ -0,0 +1,10 @@
+<?php 
+class KBB{
+	var $berat_ideal;
+
+
+	function berat_ideal($tinggi,$berat){
+	 echo "Berat Badan Idealmu : ".$this->berat_ideal = $berat / ($tinggi*$tinggi)*10000 ;
+	}
+}
+ ?>
--- index.php
+++ index.php
@@ -1,21 +1,44 @@
-<?php
-class Kelas{
-    protected $jumlah;
-    public function belajar(){
-       echo "Sedang belajar <br>";
-    }
-    public function tampil_jumlah(){
-        echo "Jumlah mahasiswa adalah ".$this->jumlah."<br>";
-    }
-}
-class StudyGroup extends Kelas{
-    public function setjumlah($jumlah){
-        $this->jumlah = $jumlah;
-    }
-}
+<!DOCTYPE html>
+<head>
+          
+    <title>Kalkulator Berat Badan</title>
+  
+</head>
+<body>
+  <form method="post" action="">
+     
+  
+   <div align="center">
+<b>Kalkulator Berat Badan</b></div>
+  
+  
+   <table align="center">
+<tr>
+    <td><input type="text" name="berat" size="10" placeholder="Tinggi"></td> 
+    <td><input type="text" name="tinggi" size="10" placeholder="Berat"></td>
+    <td><select name="jk">
+     <option name="p">P</option>
+     <option name="l">L</option>
 
-$d34102 = new StudyGroup();
-//$d34102->jumlah = 5;
-$d34102->setjumlah(20);
-$d34102->tampil_jumlah();
-?>
+    <td><input type="submit" name="submit" value="OK"></td>
+      
+    </tr>
+</table>
+</form>
+</br>
+  <center>
+  <?php
+   include "KBB.php";
+
+   $hasil = new KBB;
+
+   if (isset($_POST['submit'])){
+    $hasil -> berat_ideal($_POST['tinggi'],$_POST['berat'],$_POST['jk']);
+    echo $hasil -> berat_ideal(); ;
+   }
+
+  ?>
+  </center>
+</body>
+  
+</html>
